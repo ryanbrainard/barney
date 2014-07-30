8a75d259f882069107e9a65ea3c277937ec99d13
Ryan Brainard <ryan.brainard@gmail.com>
2014-07-29 23:33:31 -0700

    Use generic request function

--- barney/HerokuApi.swift
+++ barney/HerokuApi.swift
@@ -26,15 +26,14 @@ class HerokuApi {
     }
     
     func apps(handler: (NSArray) -> Void) {
-        return requestArray("/apps", handler)
+        return request("/apps", handler)
     }
     
     func builds(appName: String, handler: (NSDictionary) -> Void) {
         return request("/apps/\(appName)/builds", handler)
     }
-    
-    // TODO: hack until i figureout swift generics/covariance. unify with request() 
-    private func requestArray(resource: String, handler: (NSArray) -> Void) {
+  
+    private func request<R>(resource: String, handler: (R) -> Void) {
         let url = baseUrl.URLByAppendingPathComponent(resource)
         let request = NSMutableURLRequest(URL: url)
         request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData
@@ -48,32 +47,7 @@ class HerokuApi {
             }
             
             var jsonError: NSError?
-            let json = NSJSONSerialization.JSONObjectWithData(jsonData, options: nil, error: &jsonError) as NSArray
-            
-            if self.verbose {
-                println(json)
-                println(jsonError)
-            }
-            
-            handler(json)
-        }
-    }
-    
-    private func request(resource: String, handler: (NSDictionary) -> Void) {
-        let url = baseUrl.URLByAppendingPathComponent(resource)
-        let request = NSMutableURLRequest(URL: url)
-        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData
-        request.setValue("application/vnd.heroku+json; version=3", forHTTPHeaderField: "Accept")
-        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(httpResponse, jsonData, httpError) in
-            if self.verbose {
-                println(httpResponse)
-                println(NSString(data: jsonData, encoding: NSUTF8StringEncoding))
-                println(httpError)
-            }
-            
-            var jsonError: NSError?
-            let json = NSJSONSerialization.JSONObjectWithData(jsonData, options: nil, error: &jsonError) as NSDictionary
+            let json = NSJSONSerialization.JSONObjectWithData(jsonData, options: nil, error: &jsonError) as R
             
             if self.verbose {
                 println(json)
