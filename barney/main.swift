5b1b162cb4ea976d338b243d6a450fea01348ad1
Ryan Brainard <ryan.brainard@gmail.com>
2014-07-29 22:16:29 -0700

    Initial Commit

---
+++ barney/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  barney
+//
+//  Created by Ryan Brainard on 7/29/14.
+//  Copyright (c) 2014 Ryan Brainard. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
